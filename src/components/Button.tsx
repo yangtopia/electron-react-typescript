ab587b60a7135db92a448f82886f7ea0e566f7c0
Heechang Yang <yangtopia@nexivil.com>
2020-06-04 14:50:16 +0900

    FIX

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Button = () => {
-  return <button>Button</button>;
+const Button: React.FC = () => {
+  return <button type="button">Button</button>;
 };
 
 export default Button;
