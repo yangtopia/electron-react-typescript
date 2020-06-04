1ee71faeaa015a2196d87dfb5fdf2e09f6675bc9
Heechang Yang <yangtopia@nexivil.com>
2020-06-04 14:27:15 +0900

    INITIAL COMMIT

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Button from './components/Button';
+
+const mainElement = document.createElement('div');
+document.body.appendChild(mainElement);
+
+const App = () => {
+  return (
+    <>
+      <h1>Hi from a react app!</h1>
+      <Button />
+    </>
+  );
+};
+
+ReactDom.render(<App />, mainElement);
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = () => {
+  return <button>Hi from a react app!</button>;
+};
+
+export default Button;
