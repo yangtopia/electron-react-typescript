5befbca019455b06670fb2154614ed196be45172
yangtopia <yangtopia@gmail.com>
2020-06-09 00:13:34 +0900

    babylonjs 구현

---
+++ src/renderer/components/SceneComponent.tsx
@@ -0,0 +1,90 @@
+import React, { useRef, useState, useEffect } from 'react';
+import {
+  Engine,
+  Scene,
+  EngineOptions,
+  SceneOptions,
+  Nullable,
+} from '@babylonjs/core';
+import { fromEvent } from 'rxjs';
+import styled from 'styled-components';
+
+const Canvas = styled.canvas`
+  width: 100%;
+  height: 100%;
+`;
+
+interface Props {
+  antialias?: boolean;
+  engineOptions?: EngineOptions;
+  adaptToDeviceRatio?: boolean;
+  sceneOptions?: SceneOptions;
+  onSceneReady: (scene: Scene) => void;
+  onRender: (scene: Scene) => void;
+  id: string;
+}
+
+const SceneComponent: React.FC<Props> = ({
+  antialias,
+  engineOptions,
+  adaptToDeviceRatio,
+  sceneOptions,
+  onSceneReady,
+  onRender,
+  ...rest
+}) => {
+  const canvasRef = useRef<Nullable<HTMLCanvasElement>>(null);
+
+  const [loaded, setLoaded] = useState(false);
+  const [scene, setScene] = useState<Nullable<Scene>>(null);
+
+  useEffect(() => {
+    const subsc = fromEvent(window, 'resize').subscribe(() => {
+      if (scene) {
+        scene.getEngine().resize();
+      }
+    });
+
+    return () => {
+      subsc.unsubscribe();
+    };
+  }, [scene]);
+
+  useEffect(() => {
+    if (!loaded) {
+      setLoaded(true);
+      const engine = new Engine(
+        canvasRef.current,
+        antialias,
+        engineOptions,
+        adaptToDeviceRatio,
+      );
+      const currentScene = new Scene(engine, sceneOptions);
+      setScene(currentScene);
+      if (currentScene.isReady()) {
+        onSceneReady(currentScene);
+      } else {
+        currentScene.onReadyObservable.addOnce((eventScene) =>
+          onSceneReady(eventScene),
+        );
+      }
+
+      engine.runRenderLoop(() => {
+        if (typeof onRender === 'function') {
+          onRender(currentScene);
+        }
+        currentScene.render();
+      });
+    }
+
+    return () => {
+      if (scene !== null) {
+        scene.dispose();
+      }
+    };
+  }, [canvasRef]);
+
+  return <Canvas ref={canvasRef} {...rest} />;
+};
+
+export default SceneComponent;
--- src/renderer/containers/MainContainer.tsx
+++ src/renderer/containers/MainContainer.tsx
@@ -1,17 +1,87 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import {
+  FreeCamera,
+  Vector3,
+  HemisphericLight,
+  MeshBuilder,
+  Mesh,
+  Scene,
+} from '@babylonjs/core';
+import styled from 'styled-components';
 
-import Button from '@components/Button';
+import SceneComponent from '@components/SceneComponent';
 import { selectIsLoggedIn } from '@store/auth';
+
+const Wrap = styled.div`
+  position: relative;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  position: fixed;
+  left: 20px;
+  top: 0;
+  color: #fff;
+`;
+
+let box: Mesh;
 
 const MainContainer = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  const onSceneReady = (scene: Scene) => {
+    // This creates and positions a free camera (non-mesh)
+    const camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene);
+
+    // This targets the camera to scene origin
+    camera.setTarget(Vector3.Zero());
+
+    const canvas = scene.getEngine().getRenderingCanvas() as HTMLElement;
+
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
+
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+    // Our built-in 'box' shape.
+    box = MeshBuilder.CreateBox('box', { size: 2 }, scene);
+
+    // Move the box upward 1/2 its height
+    box.position.y = 1;
+
+    // Our built-in 'ground' shape.
+    MeshBuilder.CreateGround('ground', { width: 6, height: 6 }, scene);
+  };
+
+  const onRender = (scene: Scene) => {
+    if (box !== undefined) {
+      const deltaTimeInMillis = scene.getEngine().getDeltaTime();
+
+      const rpm = 10;
+      box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+    }
+  };
+
   return (
-    <>
-      <h1>Hi from a react app!! {`${isLoggedIn}`}</h1>
-      <h1>{process.env.ENV}</h1>
-      <Button />
-    </>
+    <Wrap>
+      <Title>
+        Electron React Typescript{' '}
+        <span role="img" aria-label="emoji">
+          ❤️
+        </span>{' '}
+        BabylonJS
+      </Title>
+      <SceneComponent
+        onRender={onRender}
+        onSceneReady={onSceneReady}
+        id="canvas"
+      />
+    </Wrap>
   );
 };
 
